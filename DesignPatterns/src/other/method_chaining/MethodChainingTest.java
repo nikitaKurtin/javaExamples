93491e22debd2e8936e04b3165ffae379431f1da
Gal <gal.yedidovich@gmail.com>
2017-09-24 17:01:00 +0300

    added method chaining

--- DesignPatterns/src/other/method_chaining/MethodChainingTest.java
+++ DesignPatterns/src/other/method_chaining/MethodChainingTest.java
@@ -2,6 +2,12 @@ package other.method_chaining;
 
 public class MethodChainingTest {
     public static void main(String[] args) {
+        Integer a = 0;
+        int length = new Integer(0).toString().concat(" is zero").length();
 
+        new Person()
+                .setAge(30)
+                .setName("John")
+                .printPerson();
     }
 }
